2cb2b9912336b88a3d44518b7e0a7e74bdcc6fab
darlingof02 <295226786@qq.com>
2022-02-10 13:07:59 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'billshare_frontend',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;

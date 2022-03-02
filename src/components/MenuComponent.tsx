7ce1a72412084d4aa6057705420bdb9a684a0a6f
darlingof02 <295226786@qq.com>
2022-03-01 22:53:12 -0800

    menu-raw

--- src/components/MenuComponent.tsx
+++ src/components/MenuComponent.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
-import { IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonRouterOutlet } from '@ionic/react';
+import { IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonRouterOutlet, IonAvatar, IonLabel, IonRow, IonCol, IonIcon } from '@ionic/react';
+import './MenuComponent.css'
 
 const MenuComponent: React.FC = () => (
   <>
     <IonMenu side="start" menuId="first" contentId="main">
       <IonHeader>
-        <IonToolbar color="primary">
-          <IonTitle>Start Menu</IonTitle>
+        <IonToolbar >
+          <IonTitle> Account </IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
+        <IonRow className="vertical-align-content" style={{height: "15%"}}>
+            <IonCol size='6'>
+                <IonAvatar className='item-avatar'>
+                    <img src="https://source.unsplash.com/gySMaocSdqs/600x300" alt="cat" />
+                </IonAvatar>
+            </IonCol>
+            <IonCol size='6' style={{padding: 40}}>
+                Name
+            </IonCol>
+
+        </IonRow>
+        <IonItem />
+            
+
         <IonList>
-          <IonItem>Menu Item</IonItem>
-          <IonItem>Menu Item</IonItem>
-          <IonItem>Menu Item</IonItem>
-          <IonItem>Menu Item</IonItem>
-          <IonItem>Menu Item</IonItem>
+            <IonItem>
+                <IonIcon name='person' slot='start' />
+                <IonLabel>Profile</IonLabel>
+            </IonItem>
+            <IonItem>Activity</IonItem>
+            <IonItem>Notification</IonItem>
+            <IonItem>Setting</IonItem>
+            <IonItem>Logout</IonItem>
         </IonList>
       </IonContent>
     </IonMenu>

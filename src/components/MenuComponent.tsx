c4d639ee61d3f6fc77bdd3eb92914b7c875b1381
darlingof02 <295226786@qq.com>
2022-03-02 14:28:05 -0800

    menu

--- src/components/MenuComponent.tsx
+++ src/components/MenuComponent.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonRouterOutlet, IonAvatar, IonLabel, IonRow, IonCol, IonIcon } from '@ionic/react';
+import { IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonRouterOutlet, IonAvatar, IonLabel, IonRow, IonCol, IonIcon, IonButton, IonFooter } from '@ionic/react';
 import './MenuComponent.css'
+import { notifications, person, reader, settings } from 'ionicons/icons';
+import Authentication from '../api/Authentication';
+import { useLocalStorage } from '../api/MyHooks';
 
 const MenuComponent: React.FC = () => (
+
+
+
   <>
     <IonMenu side="start" menuId="first" contentId="main">
       <IonHeader>
         <IonToolbar >
-          <IonTitle> Account </IonTitle>
+          <IonTitle className='ion-text-center'> Account </IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
+      <IonContent className='ion-padding'>
         <IonRow className="vertical-align-content" style={{height: "15%"}}>
             <IonCol size='6'>
                 <IonAvatar className='item-avatar'>
@@ -22,20 +28,33 @@ const MenuComponent: React.FC = () => (
             </IonCol>
 
         </IonRow>
-        <IonItem />
+        
             
 
         <IonList>
-            <IonItem>
-                <IonIcon name='person' slot='start' />
+          {/* <IonItem /> */}
+            <IonItem routerLink={`/profile/${localStorage.getItem('email')}`}>
+                <IonIcon icon={person} slot='start' />
                 <IonLabel>Profile</IonLabel>
             </IonItem>
-            <IonItem>Activity</IonItem>
-            <IonItem>Notification</IonItem>
-            <IonItem>Setting</IonItem>
-            <IonItem>Logout</IonItem>
+            <IonItem routerLink={`/activity/${localStorage.getItem('email')}`}>
+                <IonIcon icon={reader} slot='start' />
+                <IonLabel>Activity</IonLabel>
+            </IonItem>
+            <IonItem routerLink={`/notification/${localStorage.getItem('email')}`}>
+                <IonIcon icon={notifications} slot='start' />
+                <IonLabel>Notification</IonLabel>
+            </IonItem>
+            <IonItem routerLink={`/Setting/${localStorage.getItem('email')}`}>
+                <IonIcon icon={settings} slot='start' />
+                <IonLabel>Setting</IonLabel>
+            </IonItem>
         </IonList>
+        
       </IonContent>
+      <IonFooter>
+          <IonButton expand='block'>Logout</IonButton>
+        </IonFooter>
     </IonMenu>
 
 
